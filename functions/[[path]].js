22a24f3622118e4a06655414f34e19e54ac6777c
awovkj <147908510+awovkj@users.noreply.github.com>
2026-03-19 17:50:41 +0800

    jj

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,10 +1,10 @@
 let urls = [
-    'https://edgeone.awovkj.com#EdgeOne CDN',
+    // 'https://edgeone.awovkj.com#EdgeOne CDN',
     'https://edge.awovkj.com#EdgeOne CDN',
     // 'https://ali.blog.cmliussss.com#Ali CDN',
     // 'https://fastly.blog.cmliussss.com#Fastly CDN',
     'https://blog.awovkj.com#Vercel CDN',
-    'https://vercel.awovkj.com#Vercel CDN',
+    // 'https://vercel.awovkj.com#Vercel CDN',
     'https://cf.awovkj.com#CF CDN',
     'https://netlify.awovkj.com#Netlify CDN'
     ];
